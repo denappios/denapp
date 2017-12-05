de3cc43d4bfc9e67000b75eeaa78af2d89086901
Edaurdo Pelorca <pelorca@gmail.com>
2017-12-05 20:25:43 -0200

    Tela Login

--- DenApp/Pods/Target Support Files/Pods-DenApp/Pods-DenApp-frameworks.sh
+++ DenApp/Pods/Target Support Files/Pods-DenApp/Pods-DenApp-frameworks.sh
@@ -102,14 +102,12 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/IQKeyboardManagerSwift/IQKeyboardManagerSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/KeychainSwift/KeychainSwift.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/Material/Material.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/Motion/Motion.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/IQKeyboardManagerSwift/IQKeyboardManagerSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/KeychainSwift/KeychainSwift.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/Material/Material.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/Motion/Motion.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
