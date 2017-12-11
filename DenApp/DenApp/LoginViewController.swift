75ae48603a762317d2cb4efff630ef3b0c1c356b
Edaurdo Pelorca <pelorca@gmail.com>
2017-12-11 14:16:02 -0200

    Alteração icones tipos denuncias

--- DenApp/DenApp/LoginViewController.swift
+++ DenApp/DenApp/LoginViewController.swift
@@ -58,15 +58,14 @@ class LoginViewController: UIViewController, GIDSignInUIDelegate {
         guard let tabBarController = window??.rootViewController as? YALFoldingTabBarController else { return }
         
         let item1 = YALTabBarItem(itemImage: #imageLiteral(resourceName: "iconFire"), leftItemImage: UIImage(named: "search_icon"), rightItemImage: UIImage(named: "settings_icon"))
-        let item2 = YALTabBarItem(itemImage: #imageLiteral(resourceName: "iconAccident"), leftItemImage: UIImage(named: "search_icon"), rightItemImage: UIImage(named: "settings_icon"))
+        let item2 = YALTabBarItem(itemImage: #imageLiteral(resourceName: "iconAccident2"), leftItemImage: UIImage(named: "search_icon"), rightItemImage: UIImage(named: "settings_icon"))
         let item3 = YALTabBarItem(itemImage: #imageLiteral(resourceName: "iconWildAnimals"), leftItemImage: UIImage(named: "search_icon"), rightItemImage: UIImage(named: "settings_icon"))
+        
+         let item4 = YALTabBarItem(itemImage: #imageLiteral(resourceName: "iconCrime2"), leftItemImage: UIImage(named: "search_icon"), rightItemImage: UIImage(named: "settings_icon"))
         
         tabBarController.leftBarItems = [item1, item2]
         
-        
-        
-        let item4 = YALTabBarItem(itemImage: #imageLiteral(resourceName: "iconCrime"), leftItemImage: UIImage(named: "search_icon"), rightItemImage: UIImage(named: "settings_icon"))
-        tabBarController.rightBarItems = [item3, item4]
+          tabBarController.rightBarItems = [item3, item4]
         
         tabBarController.centerButtonImage = UIImage(named:"plus_icon")!
         tabBarController.selectedIndex = 0
@@ -85,8 +84,8 @@ class LoginViewController: UIViewController, GIDSignInUIDelegate {
         tabBarController.tabBarView.offsetForExtraTabBarItems = YALForExtraTabBarItemsDefaultOffset;
         tabBarController.tabBarView.backgroundColor = UIColor.clear
         
-        tabBarController.tabBarView.tabBarColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
-        tabBarController.tabBarView.tintColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+        tabBarController.tabBarView.tabBarColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        tabBarController.tabBarView.tintColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         tabBarController.tabBarViewHeight = YALTabBarViewDefaultHeight;
         tabBarController.tabBarView.tabBarViewEdgeInsets = YALTabBarViewHDefaultEdgeInsets;
         tabBarController.tabBarView.tabBarItemsEdgeInsets = YALTabBarViewItemsDefaultEdgeInsets;
